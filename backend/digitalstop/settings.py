3ab527e9d6650bd9867129e12723302c85eaf13e
Yerektus <qop6261@gmail.com>
2026-04-09 14:33:07 +0500

    feat: update settings.py

--- backend/digitalstop/settings.py
+++ backend/digitalstop/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'brand',
-    'customer',
-    'order',
-    'product'
+    'brand.apps.BrandConfig',
+    'customer.apps.CustomerConfig',
+    'order.apps.OrderConfig',
+    'product.apps.ProductConfig'
 ]
 
 MIDDLEWARE = [
