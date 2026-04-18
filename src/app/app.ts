635827756afcb79f679193a21f0571b358969159
Raniya Alikulova <raniya@MacBook-Pro-Raniya.local>
2026-04-18 23:16:33 +0500

    Frontend and backend connection complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('http://127.0.0.1:8000/api/v1/product/')
+      .subscribe({
+        next: (data) => {
+          this.products = data;
+          console.log('Data received:', data);
+        },
+        error: (err) => console.error('Backend is not running!', err)
+      });
+  }
 }
